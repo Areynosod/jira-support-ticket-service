18fe2ffb680617fc46de3aff0ec978db74f4ef0c
Angel Reynoso <areynosoduarte@gmail.com>
2025-07-27 17:47:24 -0400

    style: Refactor FeedbackErrorMessage and FeedbackSuccessMessage components for improved layout and consistency

--- src/components/feedback/feedback-error-message.tsx
+++ src/components/feedback/feedback-error-message.tsx
@@ -8,19 +8,28 @@ export const FeedbackErrorMessage: FC<{
   queryParams = "",
 }) => {
   return (
-    <div class="bg-red-200 p-2 rounded-lg">
-      <div class="flex items-center justify-center ">
-        <div class="w-8 h-8 bg-red-400 rounded-full flex items-center justify-center mr-4">
-          <span class="text-xl font-bold text-white">!</span>
+    <div class="w-full max-w-md mx-auto rounded-none sm:rounded-md">
+      <div class="bg-red-200 p-2 rounded-lg">
+        <div class="flex items-center justify-center ">
+          <div class="w-8 h-8 bg-red-400 rounded-full flex items-center justify-center mr-4">
+            <span class="text-xl font-bold text-white">!</span>
+          </div>
         </div>
-      </div>
-      <h3 class="text-xl font-bold text-white text-center">Submission Error</h3>
-      <p class="text-black  text-center">{message}</p>
+        <h3 class="text-xl font-bold text-white text-center">
+          Submission Error
+        </h3>
+        <p class="text-black  text-center">{message}</p>
 
-      <div className="flex justify-center">
-        <button type="button" className="btn btn-outline btn-sm">
-          Try Again
-        </button>
+        <div className="flex justify-center">
+          <button
+            type="button"
+            className="btn btn-outline btn-sm"
+            hx-get="/"
+            hx-target="#main-content"
+          >
+            Try Again
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/components/feedback/feedback-success-message.tsx
+++ src/components/feedback/feedback-success-message.tsx
@@ -7,23 +7,29 @@ export const FeedbackSuccessMessage: FC<{
   message = "Thank you for your feedback. We'll get back to you soon!",
 }) => {
   return (
-    <div class="bg-green-700 bg-opacity-20 p-6 rounded-lg">
-      <div class="flex items-center mb-4">
-        <div class="w-8 h-8 bg-green-500 rounded-full flex items-center justify-center mr-4">
-          <span class="text-sm font-bold">✓</span>
+    <div class="w-full max-w-md mx-auto rounded-none sm:rounded-md">
+      <div class="bg-green-200 p-2 rounded-lg">
+        <div class="flex items-center justify-center">
+          <div class="w-8 h-8 bg-green-400 rounded-full flex items-center justify-center mr-4">
+            <span class="text-xl font-bold text-white">✓</span>
+          </div>
         </div>
-        <h3 class="text-lg font-semibold text-green-400">Feedback Submitted</h3>
+        <h3 class="text-xl font-bold text-white text-center">
+          Feedback Submitted
+        </h3>
+        <p class="text-black text-center">{message}</p>
+
+        <div className="flex justify-center">
+          <button
+            type="button"
+            className="btn btn-outline btn-sm"
+            hx-get="/"
+            hx-target="#main-content"
+          >
+            Send Another
+          </button>
+        </div>
       </div>
-      <p class="text-green-300 mb-4">{message}</p>
-      <button
-        class="oa-button"
-        hx-params="*"
-        hx-select="#feedback-form-container"
-        hx-target="#feedback-form-container"
-        hx-swap="outerHTML"
-      >
-        Send Another
-      </button>
     </div>
   );
 };
