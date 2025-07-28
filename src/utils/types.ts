6675923753bc8c85fb284af05f2ac7946acc9721
Angel Reynoso <areynosoduarte@gmail.com>
2025-07-27 22:43:03 -0400

    feat: Update FeedbackData type and add TurnstileVerifyResponse type

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,7 +1,16 @@
-export interface FeedbackData {
+export type FeedbackData = {
   full_name: string;
   email: string;
   description: string;
   type: "Question" | "Feedback" | "Feature Request" | "Bug";
   error?: string;
-}
+};
+
+export type TurnstileVerifyResponse = {
+  success: boolean;
+  challenge_ts?: string;
+  hostname?: string;
+  "error-codes"?: string[];
+  action?: string;
+  cdata?: string;
+};
