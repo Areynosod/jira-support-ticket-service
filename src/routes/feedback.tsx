bd0bd289be1b82895f17e7ae0295644d3935dfa8
Angel Reynoso <areynosoduarte@gmail.com>
2025-07-27 22:44:09 -0400

    feat: Refactor feedback route to include project validation and Turnstile verification
    - Updated the feedback GET and POST routes to validate project queries using the project schema.
    - Integrated the getTitle function to dynamically set the page title based on the project.
    - Enhanced feedback submission with Cloudflare Turnstile verification for improved security.
    - Updated FeedbackForm to accept project as a prop for better context in error handling and success messages.

--- src/routes/feedback.tsx
+++ src/routes/feedback.tsx
@@ -6,36 +6,70 @@ import { FeedbackData } from "@/utils/types";
 import { FeedbackSuccessMessage } from "@/components/feedback/feedback-success-message";
 import { zValidator } from "@hono/zod-validator";
 import { feedbackSchema, FeedbackType } from "@/schema/feedback-schema";
+import { getTitle } from "private";
+import NoProject from "@/components/no-project";
+import { projectSchema } from "@/schema/project-schema";
+import { verifyResCloudflare } from "@/utils/functions";
 
 const feedback = new Hono();
 
-feedback.get("/", (c) => {
-  return c.html(
-    <Layout>
-      <h1 className="text-4xl font-light text-center mb-4">
-        {process.env.TITLE}
-      </h1>
-      <div>
-        <FeedbackForm data={null} errors={[]} />
-      </div>
-    </Layout>
-  );
-});
+feedback.get(
+  "/",
+  zValidator("query", projectSchema, (result, c) => {
+    if (!result.success) {
+      return c.html(<NoProject />);
+    }
+  }),
+  (c) => {
+    const { project } = c.req.valid("query");
+    const title = getTitle(project);
+
+    return c.html(
+      <Layout>
+        <h1 className="text-4xl font-light text-center mb-4">{title}</h1>
+        <div>
+          <FeedbackForm data={null} errors={[]} project={project} />
+        </div>
+      </Layout>
+    );
+  }
+);
 
 feedback.post(
   "/",
+  zValidator("query", projectSchema, (result, c) => {
+    if (!result.success) {
+      return c.html(<NoProject />);
+    }
+  }),
   zValidator("form", feedbackSchema, (result, c) => {
     if (!result.success) {
       const errors = result.error.issues.map((issue) => issue.message);
+      const project = c.req.query("project") || "";
       return c.html(
-        <FeedbackForm data={result.data as FeedbackType} errors={errors} />
+        <FeedbackForm
+          data={result.data as FeedbackType}
+          errors={errors}
+          project={project}
+        />
       );
     }
   }),
   async (c) => {
     try {
-      const formData = await c.req.formData();
+      const { project } = c.req.valid("query");
 
+      const body = await c.req.parseBody();
+      const turnstileToken = body["cf-turnstile-response"]?.toString();
+
+      const verifyData = await verifyResCloudflare(turnstileToken);
+      if (!verifyData.success) {
+        return c.html(
+          <FeedbackErrorMessage message="Verification failed. Please try again." />
+        );
+      }
+
+      const formData = await c.req.formData();
       const data: FeedbackData = {
         full_name: formData.get("full_name") as string,
         email: formData.get("email") as string,
@@ -46,11 +80,13 @@ feedback.post(
           | "Feature Request"
           | "Bug",
       };
-
-      return c.html(<FeedbackSuccessMessage />);
+      const queryParams = `/?project=${project}`;
+      return c.html(<FeedbackSuccessMessage queryParams={queryParams} />);
     } catch (error) {
+      const { project } = c.req.valid("query");
+      const queryParams = `/?project=${project}`;
       console.error("Unhandled error in feedback submission:", error);
-      return c.html(<FeedbackErrorMessage />);
+      return c.html(<FeedbackErrorMessage queryParams={queryParams} />);
     }
   }
 );
